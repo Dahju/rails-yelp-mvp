ff380523e9da63714fc77fd7aae03546edaff685
Alexandre Boyer <alexandre.boyer@kedgebs.com>
2017-11-08 18:18:21 +0100

    Have to do the front

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "06 88 75 92 32",
+    category:     "french"
+  },
+  {
+    name:         "Pizza Nico",
+    address:      "Cours Balguerie Bordeaux",
+    phone_number: "06 88 58 95 23",
+    category:     "italian"
+  },
+  {
+    name:         "Fufu",
+    address:      "Rue Saint-Rémi Bordeaux",
+    phone_number: "06 88 44 65 20",
+    category:     "japanese"
+  },
+  {
+    name:         "L'entrecôte",
+    address:      "Allee de Tourny Bordeaux",
+    phone_number: "06 74 52 89 62",
+    category:     "french"
+  },
+  {
+    name:         "Cocottes et bouchons",
+    address:      "Rue Borie Bordeaux",
+    phone_number: "06 14 52 16 95",
+    category:     "french"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
